952c2abc3ee990089030fde1c876d091386bcb94
Dina Youssef <dina.youssef10@gmail.com>
2022-12-01 19:57:22 +0100

    Optimized solution
    Signed-off-by: Dina Youssef <dina.youssef10@gmail.com>

--- solutions/day_1.py
+++ solutions/day_1.py
@@ -1,28 +1,20 @@
 from helper import load_input
 
 
-def process_input(data: str):
-    split_data = data.split("\n")
-    elves = []
-    elf = []
-    last_item = split_data[-1]
-    for s in split_data:
-        if s == last_item:
-            elf.append(int(s))
-            elves.append(elf)
-        elif s != "":
-            elf.append(int(s))
-        else:
-            elves.append(list(elf))
-            elf.clear()
-    return elves
+def process_input(raw_elf_calories: str) -> list[list[str]]:
+    calories = raw_elf_calories.split("\n\n")
+    elf_calories = [list(filter(None, elf.split("\n"))) for elf in calories]
+
+    return elf_calories
+
+
+def aggregate_calories(elf_calories: list[list[str]]) -> list[int]:
+    return [sum(int(item) for item in calories) for calories in elf_calories]
 
 
 def solution(data: str) -> int:
     elves = process_input(data)
-    calories_sum = []
-    for elf in elves:
-        calories_sum.append(sum(elf))
+    calories_sum = aggregate_calories(elves)
 
     calories_sum.sort(reverse=True)
     return calories_sum[0], sum(calories_sum[:3])
